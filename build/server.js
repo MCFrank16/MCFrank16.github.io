969e2ed98262eb499da10fadc9f1f008e4f9b14f
Frank C. Mutabazi <mecfrank16@gmail.com>
2018-11-10 19:14:54 +0200

    changes

--- build/server.js
+++ build/server.js
@@ -1,19 +1,28 @@
 'use strict';
 
+var _Courier = require('./src/controllers/Courier');
+
+var _Courier2 = _interopRequireDefault(_Courier);
+
 var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var app = (0, _express2.default)(); // server.js
-
+// server.js
+var app = (0, _express2.default)();
 
 app.use(_express2.default.json());
 
-app.get('/', function (req, res) {
-  return res.status(200).send({ 'message': 'YAY! Congratulations! Your first endpoint is working' });
-});
+app.post('/api/v1/Parcels', _Courier2.default.create);
+app.get('/api/v1/Parcels', _Courier2.default.getAll);
+app.get('/api/v1/Parcels/:id', _Courier2.default.getOne);
+app.put('/api/v1/Parcels/:id', _Courier2.default.update);
+app.delete('/api/v1/Parcels/:id', _Courier2.default.delete);
+app.get('/api/v1/Users/:UserID', _Courier2.default.getUser);
 
 app.listen(9000);
 console.log('app running on port ', 9000);
+
+module.exports = app;
